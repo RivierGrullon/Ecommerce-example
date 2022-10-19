a81befa9940aff53a63c3489671b939ef90407e6
code.bryan <brya0196@gmail.com>
2022-10-19 17:29:07 -0400

    feat: testing configured

---
+++ src/app.spec.ts
@@ -0,0 +1,7 @@
+import { describe, test, expect } from '@jest/globals';
+
+describe('Configuration example', () => {
+  test('Should return correct', () => {
+    expect(true).toBeTruthy();
+  });
+})
