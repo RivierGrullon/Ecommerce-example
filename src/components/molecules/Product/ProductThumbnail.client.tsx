8ce595947c2265e320c75db61accc712266d8223
RivierGrullon <grullonrivier@gmail.com>
2022-10-27 00:43:39 -0300

    chore: code format

--- src/components/molecules/Product/ProductThumbnail.client.tsx
+++ src/components/molecules/Product/ProductThumbnail.client.tsx
@@ -1,27 +1,35 @@
 import { Button } from "@components/atoms/Button/index";
 import { Icon } from "@components/atoms/Icons/index";
 import { Image, Props } from "@components/atoms/Image/index";
-import {useState} from "react"
-
+import { useState } from "react";
 
 interface ThumbnailProps extends Props {
   liked?: boolean;
 }
 
-export function ProductThumbnail({ src, alt, className, liked }: ThumbnailProps): JSX.Element {
-
+export function ProductThumbnail({
+  src,
+  alt,
+  className,
+  liked,
+}: ThumbnailProps): JSX.Element {
   const [isLiked, setIsLiked] = useState(liked);
 
-  function handleCLick(){
+  function handleCLick() {
     setIsLiked(!isLiked);
   }
   return (
     <div className={`relative w-64 ${className}`}>
       <Image src={src} alt={alt} className="block"></Image>
-      <Button className={" rounded-full absolute bottom-0 right-0 bg-gray-500"} onClick={handleCLick}>
-        {
-        isLiked ? ( <Icon icon="heart" filled={true}></Icon>) : (<Icon icon="heart" className={"text-white"}></Icon>)
-        }
+      <Button
+        className={" rounded-full absolute bottom-0 right-0 bg-gray-500"}
+        onClick={handleCLick}
+      >
+        {isLiked ? (
+          <Icon icon="heart" filled={true}></Icon>
+        ) : (
+          <Icon icon="heart" className={"text-white"}></Icon>
+        )}
       </Button>
     </div>
   );
--- src/components/molecules/SearchBar/index.tsx
+++ src/components/molecules/SearchBar/index.tsx
@@ -3,20 +3,16 @@ import { Icon } from "@components/atoms/Icons/index";
 import { InputField } from "@components/atoms/InputField/index";
 
 export const SearchBar = (): JSX.Element => {
-    return (
-        <div className="flex  items-center bg-white rounded-md shadow-md">
-        <InputField
-            type="text"
-            placeholder="Search for awesome"
-            className=" rounded-full border-none"
-        />
-        <Button
-            type="button"
-            className=" bg-gray-100 rounded-full ml-0" 
-        >
-            <Icon icon="search" className="w-4 h-4" />
-        </Button>
-        </div>
-    );
-    };
-
+  return (
+    <div className="flex  items-center bg-white rounded-md shadow-md">
+      <InputField
+        type="text"
+        placeholder="Search for awesome"
+        className=" rounded-full border-none"
+      />
+      <Button type="button" className=" bg-gray-100 rounded-full ml-0">
+        <Icon icon="search" className="w-4 h-4" />
+      </Button>
+    </div>
+  );
+};
